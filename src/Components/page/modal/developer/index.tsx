1bff4c4b750702082688e06dfc67806472455640
임동균 <70128434+DongEsssss@users.noreply.github.com>
2024-09-01 21:12:59 +0900

    modal(intro, developer) update

--- src/Components/page/modal/developer/index.tsx
+++ src/Components/page/modal/developer/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import './intro.scss';
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Slide } from '@mui/material';
+import { Dialog, DialogTitle, DialogContent, DialogContentText, Button, Slide, Typography } from '@mui/material';
 import { TransitionProps } from '@mui/material/transitions';
+import help_img from '../../../../asset/image/help-developer.jpg'
+import { IoIosCloseCircleOutline } from "react-icons/io";
 
 interface DialogProps {
     open: boolean;
     onClose: () => void;
 }
+
 const Transition = React.forwardRef(function Transition(
     props: TransitionProps & {
         children: React.ReactElement<any, any>;
@@ -16,25 +18,49 @@ const Transition = React.forwardRef(function Transition(
     return <Slide direction="left" ref={ref} {...props} />;
 });
 
-const Developer = ({ open, onClose }: DialogProps) => {
+function MyDialog({ open, onClose }: DialogProps) {
     return (
-        <Dialog open={open} keepMounted TransitionComponent={Transition}
+        <Dialog
+            open={open}
+            keepMounted
+            TransitionComponent={Transition}
             onClose={onClose}
             aria-describedby="alert-dialog-slide-description"
+            sx={{
+                '& .MuiDialog-paper': {
+                    position: 'absolute',
+                    right: 0,
+                    top: 0,
+                    margin: 2,
+                    maxWidth: '800px',
+                    minWidth: '400px',
+                    width: '100%',
+                    height: '95vh',
+                    zIndex: (theme) => theme.zIndex.drawer + 1, // Ensure it's on top
+                    overflow: 'auto', // Allow scrolling within the dialog
+                },
+                '& .MuiDialog-paperScrollPaper': {
+                    maxHeight: '100vh', // Adjust as needed
+                    overflow: 'auto', // Ensure scrolling within the dialog
+                },
+            }}
         >
-            <DialogTitle>{"Use Google's location service?"}</DialogTitle>
-            <DialogContent>
+            <div className="dialog-header" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <DialogTitle>임동균은 어떤 개발자가 되고 싶은가?</DialogTitle>
+                <Button onClick={onClose} sx={{ fontSize: '30px', color: 'black' }}><IoIosCloseCircleOutline /></Button>
+            </div>
+            <DialogContent sx={{ padding: '0px 24px' }}>
                 <DialogContentText id="alert-dialog-slide-description">
-                    Let Google help apps determine location. This means sending anonymous
-                    location data to Google, even when no apps are running.
+                    <Typography variant='h6' sx={{ fontFamily: 'initial', fontWeight: 'bold' }}>" 이끌어주고 밀어주는 개발자 "</Typography>
+                    <img src={help_img} style={{ width: '550px', margin: '10px auto', display: 'flex', justifyContent: 'center' }} />
+                    <Typography variant='body1' sx={{ marginTop: '10px' }}>
+                        이런 개발자를 목표 하게 된 계기는 학부 시절에 저는 고학년이라는 이유로 조에 조장을 맡게 되었습니다.
+                        팀원이 모이고 보니 1, 2학년 학우들이 많아서 프로젝트를 진행하는 것에 있어서 난항을 겪었습니다. 그래서 저는 조장이었기에 맡은 역할을 가져가야 했다고 생각했습니다. 하지만 팀원들이 웹개발자를 목표를 두고고 있을 수도 있다는생각에 팀원들과과 다 함께 할 수 있는 프로젝트를진행하자고고 생각하였고 그 팀원들과 함께 할 수 있는 방향과 난이도로 프로젝트를 조정하였습니다. 팀원들은 처음에 프로젝트를 어떻게 진행해야 하는지 코드를 어떻게 작성해야 하는지 감을 잡지 못하였습니다. 그래서 팀원 한명 한명과 개인적인 면담을 통해 팀원들의 실력에 따라 역할 분담을 하였고, 주마다 할당치를 정하였고 계단식으로한 걸음씩씩 프로젝트를 진행했습니다. 시간이 남으면 프로젝트 코드에 주석을 달아 팀원들에게 공유하면서 팀원들에게 프로젝트에 대한 이해도와 코드에 대한 이해도를 높여주었습니다. 이를 통해 팀원들의 프로젝트에 참여도와 함께 프로그래밍에 대한 흥미를 느낄 수 있게 해주었고, 코딩 실력 또한향상할킬 수 있었습니다. 이는 10개의 조 중 1등을 하는 좋은 결과를 낳을 수 있었습니다.이 때때 저는 뿌듯함과 동시에 다 함께 나아가는 것은 어렵지만 훌륭한 일이라는 것을 느껴 혼자가 아닌 다 함께 도전할 수 있도록선·후임임 개발자들을 밀어주고 이끌어주는 함께 나아가는 개발자가 되고 싶다는 목표로 갖게 되었습니다.
+                    </Typography>
                 </DialogContentText>
             </DialogContent>
-            <DialogActions>
-                <Button onClick={onClose}>Disagree</Button>
-                <Button onClick={onClose}>Agree</Button>
-            </DialogActions>
         </Dialog>
     );
 }
 
-export default Developer;
+export default MyDialog;
--- src/Components/page/modal/intro/index.tsx
+++ src/Components/page/modal/intro/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import './intro.scss';
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Slide } from '@mui/material';
+import { Dialog, DialogTitle, DialogContent, DialogContentText, Button, Slide, Typography } from '@mui/material';
 import { TransitionProps } from '@mui/material/transitions';
+import challenge_img from '../../../../asset/image/challenge.png'
+import { IoIosCloseCircleOutline } from "react-icons/io";
 
 interface DialogProps {
     open: boolean;
@@ -20,21 +21,49 @@ const Transition = React.forwardRef(function Transition(
 
 const Intro = ({ open, onClose }: DialogProps) => {
     return (
-        <Dialog open={open} keepMounted TransitionComponent={Transition}
+        <Dialog
+            open={open}
+            keepMounted
+            TransitionComponent={Transition}
             onClose={onClose}
             aria-describedby="alert-dialog-slide-description"
+            sx={{
+                '& .MuiDialog-paper': {
+                    position: 'absolute',
+                    left: 0,
+                    top: 0,
+                    margin: 2,
+                    maxWidth: '800px',
+                    minWidth: '400px',
+                    width: '100%',
+                    height: '95vh',
+                    zIndex: (theme) => theme.zIndex.drawer + 1, // Ensure it's on top
+                    overflow: 'auto', // Allow scrolling within the dialog
+                },
+                '& .MuiDialog-paperScrollPaper': {
+                    maxHeight: '100vh', // Adjust as needed
+                    overflow: 'auto', // Ensure scrolling within the dialog
+                },
+            }}
         >
-            <DialogTitle>{"Use Google's location service?"}</DialogTitle>
-            <DialogContent>
+            <div className="dialog-header" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <DialogTitle>임동균은 어떤 사람인가?</DialogTitle>
+                <Button onClick={onClose} sx={{ fontSize: '30px', color: 'black' }}><IoIosCloseCircleOutline /></Button>
+            </div>
+            <DialogContent sx={{ padding: '0px 24px' }}>
                 <DialogContentText id="alert-dialog-slide-description">
-                    Let Google help apps determine location. This means sending anonymous
-                    location data to Google, even when no apps are running.
+                    <Typography variant='h6' sx={{ fontFamily: 'initial', fontWeight: 'bold' }}>" 도전하는 자가 아름답다. "</Typography>
+                    <img src={challenge_img} style={{ width: '550px', margin: '10px auto', display: 'flex', justifyContent: 'center' }} />
+                    <Typography variant='body2' sx={{ margin: '20px 0px' }}>
+                        이 말을 어느 영상 매체에서 보고 나서 어떠한 목표가 있다면 늘 도전하는 저에게 정말 도움과 열정을 주는 문장이었습니다.
+                        어떤 목표를 만들면 저는 늘 그것에 대해서 도전했습니다. 늘 실패뿐인 도전들이었습니다. 늘 실패만 하였기 때문에 도전을 포기하고 남들이 하는 것을 따라 하는 시도 있었습니다. 하지만 그것은 제 인생에 있어서 가장 지루하고 재미없는 시간이었다는 것을 깨닫게 되었습니다.
+                        <br /> <br />
+                        그 시기에 책에서 실패를 두려워하지 말라 그 실패들은 나 자신을 좀 더 업그레이드 해줄 것이고, 남은 삶의 발판이 될 것이라고, 그래서 저는 실패만 했지만, 다시 도전해 보자 한 번 더 벽에 부딫혀 보자는 생각으로 새로운 목표를 정하여 도전하였습니다. 이번에는 세분화하여 큰 목표를 한 번에 이루기 전에 큰 목표를 작은 목표들로 나눠 도전해 보자 그럼 성공할 수 있다고 믿었습니다. 남들과 다르게 도전하는 삶은 즐거웠고, 실패가 있어도 실망하지 않고, 문제점을 찾아 보완하는 습관을 갖게 되었고, 늘 제 생각이 옳다며 타인의 말을 듣지 않았던 점도 어른들 혹은 친구들에게 조언을 얻어 생각하는 태도도 갖게 되었습니다.
+                        <br /> <br />
+                        이후 모든 목표에 대해서 도전하는 태도가 달라졌습니다. 목표에 도달하지 못해 힘들고, 기분이 안 내킬 때도, 화가 날 때도 늘 한 번 더 한 번 더 도전해 보고, 그래도 잘 안될 때는 지인분들의 조언을 얻어 문제점에 대해서 생각하고 보완점을 찾는 태도를 가지게 되었습니다. 그러니 저의 도전은 실패보다 성공을 끌어냈고, 자신감도 생기게 되었습니다. 늘 도전은 저에게 있어서 삶의 원동력이 되었고, 새로운 도전은 늘 저를 두근거리게 합니다. 늘 겸손의 자세로 도전을 이어 나갈 것입니다.
+                    </Typography>
                 </DialogContentText>
             </DialogContent>
-            <DialogActions>
-                <Button onClick={onClose}>Disagree</Button>
-                <Button onClick={onClose}>Agree</Button>
-            </DialogActions>
         </Dialog>
     );
 }
